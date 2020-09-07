3aa43d7d2ae10e05959caa915339d03a98bee94d
Elisangela <elisangela00silva@gmail.com>
2020-09-07 18:10:39 -0300

    função atualizar e selecionar

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var app = new Vue({
     el: '#app',
     data: {
         notaTexto: "",
+        notaIndex: null,
         notas: []
     },
     methods: {
@@ -21,6 +22,24 @@ var app = new Vue({
             this.notaTexto = "";
             // text01.dataset.notaId = ''
             // text01.focus();
+        },
+        atualizar: function() {
+            let notas = window.localStorage.getItem("notas");
+            notas = JSON.parse(notas);
+        
+            this.notas[this.notaIndex].nota = this.notaTexto;
+            notas[this.notaIndex].nota = this.notaTexto;
+        
+            window.localStorage.setItem("notas", JSON.stringify(notas));
+
+            this.limpar();
+        },
+        selecionar: function(e) {
+            this.notaIndex = this.notas.findIndex(function(nota){
+                return nota.id == e.target.dataset.notaId;
+            });
+
+            this.notaTexto = this.notas[this.notaIndex].nota;
         }
     },
     created: function() {
@@ -47,28 +66,11 @@ function createDiv(nota){
 }
 
 function buscarIndexDeNota(notas){
-    return notas.findIndex(function(nota){
-        return nota.id == text01.dataset.notaId;
-    });
+
 }
 
 function atualizar(){
-    let text01 = document.getElementById("text01");
-    let direita = document.querySelector(".direita");
-    
-    let notas = window.localStorage.getItem("notas");
-    notas = JSON.parse(notas);
-
-    let notaIndex = buscarIndexDeNota(notas);
-
-    notas[notaIndex].nota = text01.value;
-
-    window.localStorage.setItem("notas", JSON.stringify(notas));
-
-    direita.innerHTML = ``;
-    exibirListaNotas(notas);
-
-    limpar();
+ 
 }
 
 function deletar(){
